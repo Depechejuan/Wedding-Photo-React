e9ea37f6824eed254ddeb1a76986e1cadb0a9958
depechejuan <depechejuan@gmail.com>
2024-02-05 19:17:19 +0100

    uploading basic front

--- src/components/Form-Input.jsx
+++ src/components/Form-Input.jsx
@@ -2,16 +2,7 @@ import { useState } from 'react';
 
 function Form() {
     const [selectedImages, setSelectedImages] = useState([]);
-    const [mediaStream, setMediaStream] = useState(null);
 
-    const openCamera = async () => {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-            setMediaStream(stream);
-        } catch (error) {
-            console.error('Error al acceder a la cámara:', error);
-        }
-    };
 
     const handleImageSelect = (e) => {
         const file = e.target.files[0];
@@ -20,25 +11,16 @@ function Form() {
         }
     };
 
-    const handleCameraButtonClick = (e) => {
-        e.preventDefault(); // Prevenir el envío por defecto del formulario
-        openCamera();
-    };
+
 
     return (
     <form className="input-form">
-        <button onClick={handleCameraButtonClick} type="button">Foto 1</button>
-        <input type="file" accept="image/*" onChange={handleImageSelect} />
-        <input type="file" accept="image/*" onChange={handleImageSelect} />
-        <input type="file" accept="image/*" onChange={handleImageSelect} />
-        <p>Prueba este también</p>
-        <input type="file" accept="image/*" capture="camera" />
-        <h3>Imágenes Subidas:</h3>
-        <ul>
-        {selectedImages.map((image, i) => (
-            <li key={i.id}>{image.name}</li>
-        ))}
-        </ul>
+        {/* Este es el que funciona, ahora falta enfrentarse a quitar el preview */}
+        <input type="file" accept="image/*" capture="camera" onChange={handleImageSelect} />
+        <input type="file" accept="image/*" capture="camera" onChange={handleImageSelect} />
+        <input type="file" accept="image/*" capture="camera" onChange={handleImageSelect} />
+
+
     </form>
     );
 }
