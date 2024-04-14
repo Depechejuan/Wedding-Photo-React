138084a8fd3720ab459a8804e7ff3c15ad576677
depechejuan <depechejuan@gmail.com>
2024-04-14 19:37:05 +0200

    more changes on landing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,10 +14,15 @@ function UploadPhotos() {
             <h1>
                 ¡Bienvenidos a la JuangyBoda!
             </h1>
-            <p>Pincha en &quot;Seleccionar Archivo&quot;, haz la foto, confirma ¡y listo!</p>
-            <p>IMPORTANTE: Foto que hagas, foto que se sube. ¡No hay vuelta atrás!. ¡Ahí está la mágia!</p>
+            <div className="instructions">
+                <p>Es vuestra misión hacer las fotos del banquete de boda, para poder crear un álbum entre todos. Lo único que tenéis que hacer es:</p>
+                <p>Pinchar en &quot;Seleccionar Archivo&quot;, hacer la foto, confirmar ¡y listo!</p>
+                <p>IMPORTANTE: Foto que hagas, foto que se sube. ¡Y no hay vuelta atrás!. ¡Ahí está la mágia!</p>
+            </div>
             <Form />
             <Warning />
+
+            <p className="bottom">Juan y Ángela</p>
         </>
     )
 }
