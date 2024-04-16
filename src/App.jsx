e9eb187d5f43f1fe22dcacfd4da5957d81f4a308
depechejuan <depechejuan@gmail.com>
2024-04-16 11:30:08 +0200

    fixingerror

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import UploadPhotos from './pages/UploadPhotos'
 import ControlPanel from './pages/ControlPanel'
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/controlpanel" element={<ControlPanel />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/wedding/:id/upload" element={<UploadPhotos />} />
             </Routes>
         </BrowserRouter>
     );
